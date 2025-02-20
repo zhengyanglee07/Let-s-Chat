89453987f6f93fa14d3cd0b8e37f1a35adebe8c6
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-02-21 02:12:30 +0800

    chore: ignore package-lock

---
+++ src/lib/services/firebaseAdmin.ts
@@ -0,0 +1,12 @@
+import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT as string
+);
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export const auth = admin.auth();
